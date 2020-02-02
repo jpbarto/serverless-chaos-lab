ba78b8cb6aa115370e58ac7c3c6b13a4e5977c43
Jason Barto <jason.p.barto@gmail.com>
2020-02-02 01:16:59 +0000

    added basic conversion capability, json -> csv

--- src/lambda.js
+++ src/lambda.js
@@ -1,12 +1,43 @@
-const failureLambda = require('failure-lambda')
+const failureLambda = require('failure-lambda');
 
-exports.handler = failureLambda(async (event, context) => {
-    console.log ('Handling event', event);
+var AWS = require('aws-sdk');
+const { parse } = require('json2csv');
+
+const fields = ['objectName', 'submissionDate', 'color', 'age'];
+const opts = { "fields": fields };
+
+
+var s3 = new AWS.S3();
+
+exports.handler = failureLambda(async (event, context, callback) => {
+    console.log ('Handling event', JSON.stringify (event));
+
+    var srcBucket = event.Records[0].s3.bucket.name;
+    // Object key may have spaces or unicode non-ASCII characters.
+    var srcKey    = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " "));
+    var dstBucket = srcBucket;
+    var dstKey    = "output/" + srcKey.replace(/input\//g, "") +".csv";
+
+    console.log ("Reading JSON file", srcKey, "in bucket", srcBucket);
+
+    const data = await s3.getObject({ Bucket: srcBucket, Key: srcKey }).promise ();
+    jsonData = JSON.parse (data.Body.toString ('utf-8'));
+    console.log ("Read data:", jsonData);
+
+    try {
+        const csvData = parse (jsonData, opts);
+        console.log("CSV data", csvData);
+        const result = await s3.putObject({ Bucket: dstBucket, Key: dstKey, Body: csvData, ContentType: 'text/csv' }).promise ();
+    } catch (err) {
+        console.error(err);
+    }
+
 
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!')
+        body: JSON.stringify('Input conversion complete')
     };
 
-    return response;
+    console.log ("Object processed");
+    callback(null, response);
 });
--- terraform.tf
+++ terraform.tf
@@ -14,16 +14,18 @@ resource "random_id" "chaos_stack" {
   byte_length = 8
 }
 
-resource "aws_s3_bucket" "input_bucket" {
-  bucket = "super-random-jasbarto-bucket-${random_id.chaos_stack.hex}"
+resource "aws_s3_bucket" "chaos_bucket" {
+  bucket = "chaos-bucket-${random_id.chaos_stack.hex}"
 }
 
 resource "aws_s3_bucket_notification" "bucket_notification" {
-  bucket = "${aws_s3_bucket.input_bucket.id}"
+  bucket = "${aws_s3_bucket.chaos_bucket.id}"
 
   lambda_function {
     lambda_function_arn = "${aws_lambda_function.chaos_lambda.arn}"
     events              = ["s3:ObjectCreated:*"]
+    filter_prefix       = "input/"
+    filter_suffix       = ".json"
   }
 }
 
@@ -66,6 +68,23 @@ resource "aws_iam_role_policy" "chaos_policy" {
         ],
         "Effect": "Allow",
         "Resource": "${aws_ssm_parameter.chaos_lambda_param.arn}"
+      },
+      {
+        "Action": [
+          "s3:GetObject",
+          "s3:PutObject"
+        ],
+        "Effect": "Allow",
+        "Resource": "${aws_s3_bucket.chaos_bucket.arn}/*"
+      },
+      {
+        "Action": [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        "Effect": "Allow",
+        "Resource": "*"
       }
     ]
   }
@@ -77,7 +96,7 @@ resource "aws_lambda_permission" "allow_bucket" {
   action        = "lambda:InvokeFunction"
   function_name = "${aws_lambda_function.chaos_lambda.arn}"
   principal     = "s3.amazonaws.com"
-  source_arn    = "${aws_s3_bucket.input_bucket.arn}"
+  source_arn    = "${aws_s3_bucket.chaos_bucket.arn}"
 }
 
 data "archive_file" "chaos_lambda_zip" {
@@ -88,6 +107,7 @@ data "archive_file" "chaos_lambda_zip" {
 
 resource "aws_lambda_function" "chaos_lambda" {
     filename = "build/chaos_lambda.zip"
+    source_code_hash = filebase64sha256("build/chaos_lambda.zip")
     function_name = "ChaosTransformer-${random_id.chaos_stack.hex}"
     handler = "lambda.handler"
     memory_size = 128
