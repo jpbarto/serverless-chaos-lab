a8e9ae8069dd4c3afe05fa42a8d1cf3db9620a35
Jason Barto <jason.p.barto@gmail.com>
2020-02-03 00:28:56 +0000

    modified data upload script to be more efficient and use sync

--- chaos/submit_input_files.sh
+++ chaos/submit_input_files.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+rm -fr tmp_data
+mkdir tmp_data
+
 for i in `seq 1 $1`;
 do
-    aws s3 cp ../sample_data.json s3://chaos-bucket-23e0fcb0737fba73/input/sample_data_$i.json
+    cp sample_data.json tmp_data/sample_data_$i.json
 done
+
+aws s3 sync tmp_data s3://chaos-bucket-23e0fcb0737fba73/input/ 1>/dev/null
