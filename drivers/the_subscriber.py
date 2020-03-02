5ed535f885d441a3dfffd4a54ff5250a0eb2a9c3
Jason Barto <jason.p.barto@gmail.com>
2020-03-02 00:10:19 +0000

    added the same keyboard interrupt handler to the subscriber

--- drivers/the_subscriber.py
+++ drivers/the_subscriber.py
@@ -4,6 +4,7 @@ import boto3
 from time import time
 from datetime import datetime as dt
 import json
+import sys
 
 from aws_resource_names import SQS_QUEUE_NAME
 
@@ -19,15 +20,19 @@ obj_limit = 2 # number of objects per second to get
 start_time = time ()
 last_print_time = time ()
 while run_flag:
-    if iter_obj_count <= obj_limit * 10:
-        resp = sqs.receive_message (QueueUrl=queue_url, WaitTimeSeconds=1, MaxNumberOfMessages=obj_limit)
-        if 'Messages' in resp:
-            for msg in resp['Messages']:
-                sqs.delete_message (QueueUrl=queue_url, ReceiptHandle=msg['ReceiptHandle'])
-                obj_count += 1
-                iter_obj_count += 1
+    try:
+        if iter_obj_count <= obj_limit * 10:
+            resp = sqs.receive_message (QueueUrl=queue_url, WaitTimeSeconds=1, MaxNumberOfMessages=obj_limit)
+            if 'Messages' in resp:
+                for msg in resp['Messages']:
+                    sqs.delete_message (QueueUrl=queue_url, ReceiptHandle=msg['ReceiptHandle'])
+                    obj_count += 1
+                    iter_obj_count += 1
 
-    if (int(time () - start_time) % 10) == 0 and (time () - last_print_time) > 10:
-        print ("{}: Retrieved {} objects for a total of {} objects".format (dt.now ().strftime ('%Y-%b-%d %H:%M:%S'), iter_obj_count, obj_count))
-        last_print_time = time ()
-        iter_obj_count = 0
+        if (int(time () - start_time) % 10) == 0 and (time () - last_print_time) > 10:
+            print ("{}: Retrieved {} objects for a total of {} objects".format (dt.now ().strftime ('%Y-%b-%d %H:%M:%S'), iter_obj_count, obj_count))
+            last_print_time = time ()
+            iter_obj_count = 0
+    except KeyboardInterrupt:
+        print ("Retrieved a total of {} objects; exiting...".format (obj_count))
+        sys.exit (0)
