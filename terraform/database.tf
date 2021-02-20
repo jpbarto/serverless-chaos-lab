1336af895b74ec8840824b69b0c56a4f90c06489
Jason Barto <jason.p.barto@gmail.com>
2021-02-20 00:57:17 +0000

    Update database.tf
    Removed provisioned read / write units per user feedback

--- terraform/database.tf
+++ terraform/database.tf
@@ -6,9 +6,7 @@
 
 resource "aws_dynamodb_table" "chaos_data_table" {
   name           = "chaos-data-${random_id.chaos_stack.hex}"
-  billing_mode   = "PROVISIONED"
-  read_capacity  = 20
-  write_capacity = 20
+  billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "symbol"
   range_key       = "entryType"
 
